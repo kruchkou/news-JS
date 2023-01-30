e6e255d31029c048df99badd44c4e811adb5a9bb
kruchkou <kru4kou@gmail.com>
2023-01-30 20:30:36 +0100

    feat: add typescript configs

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '331c4885400a407091a1921a3048e9e5', // получите свой ключ https://newsapi.org/
         });
     }
 }
